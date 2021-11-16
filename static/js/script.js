9d3ea1e0d07994dd883fc04e8751d08a361a9d5d
SanjayKH-Git <sanjayhegde92@gmail.com>
2021-11-16 20:42:23 +0530

    phone No done

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function($){
+    $(".dropdown").on('click', function() {
+    $(".section-dropdown").show();
+    });
+
+    $("#PhNo").on('click', function() {
+        $(".section-dropdown").hide();
+        $("#target").val("");
+        $("#target").attr({"type":"tel","name":"PhoneNo","pattern":"^\\d{10}$", "placeholder":"Enter Target PhoneNo"},);
+    });
+    $("#email").on('click', function() {
+        $(".section-dropdown").hide();
+        $("#target").val("");
+        $("#target").attr({"type":"email","name":"Email","pattern":"[^@\\s]+@[^@\\s]+\\.[^@\\s]+", "placeholder":"Enter Target Email"},);
+    });
+    $("#website").on('click', function() {
+        $(".section-dropdown").hide();
+        $("#target").val("");
+        $("#target").attr({"type":"url","name":"webLink","pattern":"https://.*|http://.*", "placeholder":"Enter Target URL"},);
+    });
+
+});
